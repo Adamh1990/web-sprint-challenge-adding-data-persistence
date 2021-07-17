7b894c1ed8815a99262b01c73152b25372219cbf
Adamh1990 <68036525+Adamh1990@users.noreply.github.com>
2021-07-16 19:00:22 -0500

    cleanup

--- api/resource/router.js
+++ api/resource/router.js
@@ -10,19 +10,11 @@ router.get('/', (req, res, next) => {
     .catch(err => next(err))
   })
   
-  router.get('/:id', checkForResource(), (req, res, next) => {
+  router.get('/:id', (req, res, next) => {
     Resources.findById(req.params.id)
     .then(resource => res.status(200).json(resource))
     .catch(err => next(err))
   })
-  
-  // router.post('/', validateResourceDataReqs(), (req, res, next) => {
-  //   Resources.add(req.body)
-  //   .then(() => {
-  //     res.status(201).json({message: 'resource created'})
-  //   })
-  //   .catch(err => next(err))
-  // })
 
   router.post('/', (req, res, next) => {
     Resources.add(req.body)
@@ -30,40 +22,5 @@ router.get('/', (req, res, next) => {
         .catch(err => next(err))
         }
 )
-  
-  router.put('/:id', checkForResource(), validateResourceDataReqs(), (req, res, next) => {
-    Resources.update(req.params.id, req.body)
-    .then(() => {
-      res.status(200).json({message: 'resource updated successfully'})
-    })
-    .catch(err => next(err))
-  })
-  
-  router.delete('/:id', checkForResource(), (req, res, next) => {
-    Resources.remove(req.params.id)
-    .then(() => {
-      res.status(200).json({message: 'resource deleted'})
-    })
-    .catch(err => next(err))
-  })
-  
-  function validateResourceDataReqs() {
-    return (req, res, next) => {
-      if(!req.body.name) {
-        res.status(400).json({error: 'name is a required field'})
-      }
-      else { next() }
-    }
-  }
-  
-  function checkForResource() {
-    return (req, res, next) => {
-      const requestedResource = Resources.findById(req.params.id)
-  
-      if (requestedResource) { next() }
-      else {
-        res.status(404).json({error: 'referenced resource not found'})
-      }
-    }
-  }
+
 module.exports = router
--- api/task/router.js
+++ api/task/router.js
@@ -11,77 +11,24 @@ router.get('/', (req, res) => {
       })
 })
 
-router.get('/:projectId/tasks', checkForProject(), (req, res, next) => {
+router.get('/:projectId/tasks', (req, res, next) => {
     Tasks.find(req.params.projectId)
     .then(tasks => res.status(200).json(tasks))
     .catch(err => next(err))
   })
   
   // intermediary table so task id restarts per project?
-  router.get('/:projectId/tasks/:id', checkForProject(), checkForTask(), (req, res, next) => {
+  router.get('/:projectId/tasks/:id', (req, res, next) => {
     Tasks.findById(req.params.projectId, req.params.id)
     .then(task => res.status(200).json(task))
     .catch(err => next(err))
   })
   
-  router.post('/tasks', checkForProject(), validateTaskDataReqs(), (req, res, next) => {
+  router.post('/', (req, res, next) => {
     Tasks.add(req.body)
-    .then(() => {
-      res.status(201).json({message: 'task created'})
-    })
-    .catch(err => next(err))
-  })
-  
-  router.put('/:projectId/tasks/:id', checkForProject(), checkForTask(), validateTaskDataReqs(), (req, res, next) => {
-    Tasks.update(req.params.projectId, req.params.id, req.body)
-    .then(() => {
-      res.status(200).json({message: 'task updated successfully'})
-    })
-    .catch(err => next(err))
-  })
-  
-  router.delete('/:projectId/tasks/:id', checkForProject(), checkForTask(), (req, res, next) => {
-    Tasks.remove(req.params.projectId, req.params.id)
-    .then(() => {
-      res.status(200).json({message: 'project deleted'})
-    })
-    .catch(err => next(err))
-  })
-  
-  function validateTaskDataReqs() {
-    return (req, res, next) => {
-      if(!req.body.project_id || !req.body.description) {
-        res.status(400).json({error: 'project id and description are required fields'})
-      }
-      else {
-        next()
-      }
-    }
-  }
-  
-  function checkForProject() {
-    return (req, res, next) => {
-      const requestedProject = Tasks.findById(req.params.id)
-  
-      if (requestedProject) {
-        next()
-      }
-      else{
-        res.status(404).json({error: 'referenced project not found'})
-      }
-    }
-  }
-  
-  function checkForTask() {
-    return (req, res, next) => {
-      const requestedTask = Tasks.findById(req.params.projectId, req.params.id)
-  
-      if (requestedTask) {
-        next()
-      }
-      else{
-        res.status(404).json({error: 'referenced task not found'})
-      }
-    }
-  }
+        .then(task => res.status(200).json(task))
+        .catch(err => next(err))
+        }
+)
+
 module.exports = router
